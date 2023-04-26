7551c7c98676d75b88d2b5d4f300422d97519f71
Brendan Le <bankingbrendan@gmail.com>
2023-04-26 00:06:43 -0400

    added data+plot

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # documentation: https://www.alphavantage.co/documentation/
 
 import requests
+import pandas as pd
+import matplotlib.pyplot as plt
 
 api_key = "N20TDIL2VOIJS399"
 
@@ -17,12 +19,30 @@ if choice == "1":
 
     stock_ticker = input("Type your desired stock ticker and return. ")
 
-    url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={stock_ticker}&apikey={api_key}"
-    response = requests.get(url)
-    data = response.json()
-    price = data['Global Quote']['05. price']
+    global_quote_url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={stock_ticker}&apikey={api_key}"
+    response = requests.get(global_quote_url)
+    global_quote_data = response.json()
+    day = global_quote_data["Global Quote"]["07. latest trading day"]
+    price = global_quote_data["Global Quote"]["05. price"]
 
-    print(f"The current price of {stock_ticker} as of {data['Global Quote']['07. latest trading day']} is: {price}")
+    print(f"The current price of {stock_ticker} as of {day} is: {price}")
+
+    time_series_monthly_adjusted_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol={stock_ticker}&apikey={api_key}"
+    response = requests.get(time_series_monthly_adjusted_url)
+    time_series_monthly_adjusted_data = response.json()
+
+    df = pd.DataFrame.from_dict(time_series_monthly_adjusted_data['Monthly Adjusted Time Series'], orient='index')
+    df.index = pd.to_datetime(df.index)
+
+    prices = df['5. adjusted close'].astype(float).resample('M').last()
+    axis = prices.plot(title=f"{stock_ticker} Historical Monthly Adjusted Close Prices")
+    axis.set_xlabel("Year")
+    axis.set_ylabel("PRice")
+
+    plt.savefig("stockimage.png")
+    
+    #plt.show()
+    #print(time_series_monthly_adjusted_data)
 
 elif choice == "2":
     print("You selected Option 2.")
