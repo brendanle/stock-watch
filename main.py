05461265b271fbf81231b284bd362549d35c10e3
Brendan Le <bankingbrendan@gmail.com>
2023-04-27 17:24:43 -0400

    error handling + overview function

--- main.py
+++ main.py
@@ -16,6 +16,11 @@ def get_monthly_adjusted_data(stock_ticker, api_key):
     response = requests.get(url)
     return response.json()["Monthly Adjusted Time Series"]
 
+def get_overview(stock_ticker, api_key):
+    url = f"https://www.alphavantage.co/query?function=OVERVIEW&symbol={stock_ticker}&apikey=${api_key}"
+    response = requests.get(url)
+    return response.json()
+
 print("\nSelect an option \n ==== MENU ====")
 
 menu_items = ["Stocks", "Forex", "Crypto", "Commodities", "Economic Indicators"]
@@ -29,20 +34,40 @@ if choice == "1":
 
     stock_ticker = input("Type your desired stock ticker and return. ")
 
-    # Stock Daily Quote
-    global_quote_data = get_global_quote_data(stock_ticker, api_key)
-    day = global_quote_data["07. latest trading day"]
-    price = global_quote_data["05. price"]
-    print(f"The current price of {stock_ticker} as of {day} is: {price}")
+    # Overview
+    try:
+        overview_data = get_overview(stock_ticker, "demo")
+        symbol = overview_data["Symbol"]
+        name = overview_data["Name"]
+        description = overview_data["Description"]
+        exchange = overview_data["Exchange"]
+    except KeyError as e:
+        print(f"encountered error (overview): {e}")
 
-    # Stock Full Chart
-    time_series_monthly_adjusted_data = get_monthly_adjusted_data(stock_ticker, api_key)
-    df = pd.DataFrame.from_dict(time_series_monthly_adjusted_data, orient="index")
-    df.index = pd.to_datetime(df.index)
-    prices = df["5. adjusted close"].astype(float).resample("M").last()
-    axis = prices.plot(title=f"{stock_ticker} Historical Monthly Adjusted Close Prices")
-    axis.set_xlabel("Year")
-    axis.set_ylabel("PRice")
+    # Global Quote
+    try:
+        global_quote_data = get_global_quote_data(stock_ticker, "demo")
+        day = global_quote_data["07. latest trading day"]
+        price = global_quote_data["05. price"]
+    except KeyError as e:
+        print(f"encountered error (global quote): {e}")
+
+    # Chart
+    try:
+        time_series_monthly_adjusted_data = get_monthly_adjusted_data(stock_ticker, "demo")
+        df = pd.DataFrame.from_dict(time_series_monthly_adjusted_data, orient="index")
+        df.index = pd.to_datetime(df.index)
+        prices = df["5. adjusted close"].astype(float).resample("M").last()
+        axis = prices.plot(title=f"{stock_ticker} Historical Monthly Adjusted Close Prices")
+        axis.set_xlabel("Year")
+        axis.set_ylabel("PRice")
+    except KeyError as e:
+        print(f"encountered error (chart): {e}")
+
+    try:
+        print("\n" + "[" + day + "] " + symbol + " - " + price + "\n\n" + description)
+    except:
+        print("Error printing out the details!")
 
     # Save & Show
     plt.savefig("stockimage.png")
