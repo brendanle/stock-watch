99602a74dde9a5528ce2bc9094c642a22292244c
Brendan Le <bankingbrendan@gmail.com>
2023-04-28 18:38:58 -0400

    forex addition w/ chart

--- main.py
+++ main.py
@@ -18,12 +18,17 @@ def get_monthly_adjusted_data(stock_ticker, api_key):
     return response.json()["Monthly Adjusted Time Series"]
 
 def get_overview(stock_ticker, api_key):
-    url = f"https://www.alphavantage.co/query?function=OVERVIEW&symbol={stock_ticker}&apikey=${api_key}"
+    url = f"https://www.alphavantage.co/query?function=OVERVIEW&symbol={stock_ticker}&apikey={api_key}"
     response = requests.get(url)
     return response.json()
 
 def currency_exchange_rate(first_currency, second_currency, api_key):
-    url = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={first_currency}&to_currency={second_currency}&apikey=demo"
+    url = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={first_currency}&to_currency={second_currency}&apikey={api_key}"
+    response = requests.get(url)
+    return response.json()
+
+def fx_monthly(first_currency, second_currency, api_key):
+    url = f"https://www.alphavantage.co/query?function=FX_MONTHLY&from_symbol={first_currency}&to_symbol={second_currency}&apikey=demo"
     response = requests.get(url)
     return response.json()
 
@@ -43,7 +48,6 @@ if choice == "1":
     # Overview
     if valid_ticker:
         try:
-            # overview_data = get_overview(stock_ticker, "demo")
             overview_data = get_overview(stock_ticker, api_key)
             symbol = overview_data["Symbol"]
             name = overview_data["Name"]
@@ -56,7 +60,6 @@ if choice == "1":
     # Global Quote
     if valid_ticker:
         try:
-            # global_quote_data = get_global_quote_data(stock_ticker, "demo")
             global_quote_data = get_global_quote_data(stock_ticker, api_key)
             day = global_quote_data["07. latest trading day"]
             price = global_quote_data["05. price"]
@@ -67,14 +70,13 @@ if choice == "1":
     # Chart
     if valid_ticker:
         try:
-            # time_series_monthly_adjusted_data = get_monthly_adjusted_data(stock_ticker, "demo")
             time_series_monthly_adjusted_data = get_monthly_adjusted_data(stock_ticker, api_key)
             df = pd.DataFrame.from_dict(time_series_monthly_adjusted_data, orient="index")
             df.index = pd.to_datetime(df.index)
             prices = df["5. adjusted close"].astype(float).resample("M").last()
             axis = prices.plot(title=f"{stock_ticker} Historical Monthly Adjusted Close Prices")
             axis.set_xlabel("Year")
-            axis.set_ylabel("PRice")
+            axis.set_ylabel("Price")
         except KeyError as e:
             print(f"encountered error (chart): {e}")
             valid_ticker = False
@@ -96,13 +98,12 @@ if choice == "1":
 
 elif choice == "2":
 
-    #forex
     first_currency = input("Enter the first currency index you would like to see the exchange rate for. ")
     second_currency = input("Enter the second currency index you would like to see the exchange rate for. ")
 
     if valid_ticker:
         try:
-            currency_exchange_rate_data = currency_exchange_rate(first_currency, second_currency, "demo")
+            currency_exchange_rate_data = currency_exchange_rate(first_currency, second_currency, api_key)
             from_currency_code = currency_exchange_rate_data["Realtime Currency Exchange Rate"]["1. From_Currency Code"]
             to_currency_code = currency_exchange_rate_data["Realtime Currency Exchange Rate"]["3. To_Currency Code"]
             exchange_rate = currency_exchange_rate_data["Realtime Currency Exchange Rate"]["5. Exchange Rate"]
@@ -112,11 +113,30 @@ elif choice == "2":
 
     if valid_ticker:
         try:
+            fx_monthly_data = fx_monthly(first_currency, second_currency, api_key)
+            df = pd.DataFrame.from_dict(fx_monthly_data["Time Series FX (Monthly)"], orient="index")
+            df.index = pd.to_datetime(df.index)
+            prices = df["4. close"].astype(float).resample("M").last()
+            axis = prices.plot(title=f"{first_currency}/{second_currency} Time Series FX")
+            axis.set_xlabel("Year")
+            axis.set_ylabel("Price")
+        except KeyError as e:
+            print(f"encountered error (chart): {e}")
+            valid_ticker = False
+
+    if valid_ticker:
+        try:
             print("\n----------------------------")
-            print("\n" + "The current exchange rate between " + first_currency + " and " + second_currency + " is " + exchange_rate + ".\n")
+            print("\n" + "The current exchange rate between " + first_currency + "/" + second_currency + " is " + exchange_rate + ".\n")
             print("----------------------------")
         except:
             print("Error printing out the details!")
+
+    if valid_ticker:
+        plt.savefig("stockimage.png")
+        plt.show()
+    else:
+        print("Couldn't plot.")
 
 elif choice == "3":
     print("You selected Option 3.")
